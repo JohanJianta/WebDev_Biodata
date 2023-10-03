31b9e961ab41027db48b2afc2d819b428e941584
JohanJianta <johanreinaldojianta@gmail.com>
2023-10-03 08:30:27 +0800

    buat biodata

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import avatar from './image/avatar.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body className='App-body'>
+        <div className="Body-kiri">
+          <img src={avatar} className="Header-logo" alt="avatar" />
+          <li>Pria</li>
+          <li>Mahasiswa</li>
+        </div>
+
+        <div className='Body-kanan'>
+          <div>
+            <p className='Header-name'>
+              Johan Reinaldo Jianta
+            </p>
+            <hr className='Garis-name'></hr>
+          </div>
+          <div className='List-info'>
+            <li>Angkatan 2022</li>
+            <li>Informatika - Full Stack Development</li>
+            <li>Jalur Sukses: Corporate Entrepreneurship</li>
+            <li>Hobi: Tidur</li>
+          </div>
+        </div>
+      </body>
+
+      <div className='Body-bawah'>
+        <hr></hr>
+        <p>Riwayat Organisasi:</p>
+        <li>Student Union Informatika 2023</li>
+      </div>
     </div>
   );
 }
