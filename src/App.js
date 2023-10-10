dde2483d901df4abd9d533ae3c093f23c63d7e3b
JohanJianta <johanreinaldojianta@gmail.com>
2023-10-10 08:55:24 +0800

    ubah biodata ke format react

--- src/App.js
+++ src/App.js
@@ -1,47 +1,73 @@
+import React, { useState } from 'react';
 import avatar from './image/avatar.png';
-import { useState } from 'react';
 import './App.css';
 
 function App() {
   const [circle, setCircle] = useState(false);
 
-  return (
-    <div className="App">
-      <body className='App-body'>
-        <div className="Body-kiri">
-          <img src={avatar} className={`Header-logo ${circle ? 'lingkaran' : ''}`} alt="avatar" />
-          <li>Pria</li>
-          <li>Mahasiswa</li>
-        </div>
+  const createElement = (type, props, children) =>
+    React.createElement(type, props, children);     //  set object createElement untuk mempermudah nesting
 
-        <div className='Body-kanan'>
-          <div>
-            <p className='Header-name'>
-              Johan Reinaldo Jianta
-            </p>
-            <hr className='Garis-name'></hr>
-          </div>
-          <div className='List-info'>
-            <li>Angkatan 2022</li>
-            <li>Informatika - Full Stack Development</li>
-            <li>Jalur Sukses: Corporate Entrepreneurship</li>
-            <li>Hobi: Tidur</li>
-          </div>
-        </div>
-      </body>
+  const image = createElement('img', {
+    src: avatar,
+    className: `Header-logo ${circle ? 'lingkaran' : ''}`,    //  class 'lingkaran' hanya muncul ketika tombol ditekan
+    alt: 'avatar',
+  });
 
-      <div className='Body-bawah'>
-        <hr></hr>
-        <div className='Bawah-child'>
-          <p>Riwayat Organisasi:</p>
-          <li>Student Union Informatika 2023</li>
-        </div>
-        <div className='Bawah-child Tombol-container'>
-          <button onClick={() => setCircle(prev => !prev)}>Ganti Border</button>
-        </div>
-      </div>
-    </div>
+  const bodyKiri = createElement(
+    'div',
+    { className: 'Body-kiri' },
+    [image, createElement('li', null, 'Pria'), createElement('li', null, 'Mahasiswa')]
   );
+
+  const containerName = createElement(
+    'div',
+    null,
+    [
+      createElement('p', { className: 'Header-name' }, 'Johan Reinaldo Jianta'),
+      createElement('hr', { className: 'Garis-name' }),
+    ]
+  );
+
+  const listInfo = createElement(
+    'div',
+    { className: 'List-info' },
+    [
+      createElement('li', null, 'Angkatan 2022'),
+      createElement('li', null, 'Informatika - Full Stack Development'),
+      createElement('li', null, 'Jalur Sukses: Corporate Entrepreneurship'),
+      createElement('li', null, 'Hobi: Tidur'),
+    ]
+  );
+
+  const bodyKanan = createElement('div', { className: 'Body-kanan' }, [containerName, listInfo]);
+
+  const bodyAtas = createElement('body', { className: 'App-body' }, [bodyKiri, bodyKanan]);
+
+  const containerRiwayat = createElement(
+    'div',
+    { className: 'Bawah-child' },
+    [
+      createElement('p', null, 'Riwayat Organisasi:'),
+      createElement('li', null, 'Student Union Informatika 2023'),
+    ]
+  );
+
+  const containerTombol = createElement(
+    'div',
+    { className: 'Bawah-child Tombol-container' },
+    [
+      createElement('button', { onClick: () => setCircle(prev => !prev) }, 'Ganti Border'),
+    ]
+  );
+
+  const bodyBawah = createElement('div', { className: 'Body-bawah' }, [
+    createElement('hr', null),
+    containerRiwayat,
+    containerTombol,
+  ]);
+
+  return createElement('div', { className: 'App' }, [bodyAtas, bodyBawah]);
 }
 
 export default App;
